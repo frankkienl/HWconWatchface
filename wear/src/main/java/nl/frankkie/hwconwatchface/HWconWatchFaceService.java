d59068d641d72a36f2ddbd263c91530c85fb4e1a
Frank Bouwens <frankkie12345@gmail.com>
2014-12-22 23:04:28 +0100

    Initial Import

---
+++ wear/src/main/java/nl/frankkie/hwconwatchface/HWconWatchFaceService.java
@@ -0,0 +1,10 @@
+package nl.frankkie.hwconwatchface;
+
+import android.support.wearable.watchface.CanvasWatchFaceService;
+
+/**
+ * Created by FrankkieNL on 22-12-2014.
+ */
+public class HWconWatchFaceService extends CanvasWatchFaceService {
+    //https://developer.android.com/training/wearables/watch-faces/service.html
+}
